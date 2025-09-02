652eb0cdac9b7a10778f7689238f59a06aeaf56e
Leelakrishna Muppandam <leelakrishna591@gmail.com>
2025-09-03 00:26:51 +0530

    Update log_file_monitor.sh

--- basic/log_file_monitor.sh
+++ basic/log_file_monitor.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# log_file_monitor.sh - Basic log monitoring script
-
-# Default log file (can be changed)
+# ============================================
+# Script Name : log_file_monitor.sh
+# Description : Basic log monitoring script
+# Author      : Leelakrishna
+# ============================================
+echo "------------Logs-monitor-------------------"
 LOG_FILE="/var/log/syslog"
 
-# Check if log file exists
+# Checking if log file exists
 if [ ! -f "$LOG_FILE" ]; then
     echo "❌ Log file $LOG_FILE not found!"
-    exit 1
 fi
 
 echo "------------Log File Monitor-------------------"
 echo "🔹 Last 10 lines of $LOG_FILE:"
 tail -n 10 "$LOG_FILE"
-
-echo
-echo "⚠️  Errors/Warnings in the last 50 lines:"
-# Search last 50 lines for error keywords (case-insensitive)
+echo "----⚠️  Errors/Warnings in the last 50 lines:----------"
 tail -n 50 "$LOG_FILE" | grep -i -E "error|fail|warn" || echo "No errors found."
